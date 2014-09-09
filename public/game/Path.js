5caa887409e1cc49f00c0a971748a58dfbf292a6
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-09-09 22:53:43 +0400

    cleanup

--- public/game/Path.js
+++ public/game/Path.js
@@ -1,12 +1,12 @@
+/* NOT YET WORKING! LOOK AT game/demo/Game.js to help me integrate*/
+
 Path = function (game, tilemap, walkables, sprite){
 	this.game = game;
-	console.log(this.game);
 	this.marker = null;
 	this.sprite = sprite;
 	this.tilemap = tilemap.map;
 	this.layer = tilemap.layer3;
 	this.pathfinder = this.game.plugins.add(Phaser.Plugin.PathFinderPlugin);
-	console.log(tilemap.map.layers[2].data);
 	this.pathfinder.setGrid(tilemap.map.layers[2].data, walkables);
 };
 
@@ -21,5 +21,21 @@ Path.prototype = {
 	getWorldCoords: function (coord){
 		var currentTile = this.tilemap.getTile(coord.x, coord.y); 
 		return {x:currentTile.worldX, y: currentTile.worldY};
+	},
+	
+	cb: function(path){
+		path = path || [];
+		return path;
+		var tween = this.game.add.tween(this.player.sprite.body);
+		for(var i =0; i < path.length; i++){
+			var wc = this.getWorldCoords(path[i]);
+			tween.to({x: wc.x, y:wc.y}, 100, Phaser.Easing.Linear.None);
+		}
+		tween.interpolation(Phaser.Math.bezierInterpolation)
+		.start();
+	},
+	
+	doSomething: function (){
+			this.path.findPathTo(this.game.input.activePointer.worldX, 			this.game.input.activePointer.worldY, this.cb);
 	}
 };
