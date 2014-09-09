da64d10fd95d7449078608c27b4c51f3cbf07afd
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-09-09 12:44:29 +0400

    faster handling of sprite motion + cleanups
    -removes double detection of key downs for stops
    -creates prototype fns instead of ‘this, that’ IIFEs.

--- public/game/Player.js
+++ public/game/Player.js
@@ -10,7 +10,7 @@ Player.prototype = {
 	preload: function () {
 		this.game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 	},
-	
+
 	addAnimations: function() {
 		this.sprite.animations.add('left', [0, 1, 2, 3], 10, true);
 		this.sprite.animations.add('right', [5, 6, 7, 8], 10, true);
@@ -18,72 +18,78 @@ Player.prototype = {
 		this.sprite.animations.add('down', [4], 10, true);
 	},
 
-	create: function () {
+	createPlayerGroup: function () {
 		this.sprite = game.add.sprite(game.world.width/2, game.world.height/2, 'dude');
+		this.label = this.game.add.text(this.sprite.x, 	this.sprite.y, this.name, { font: '8pt Helvetica Neue', fill: '#000' });
+		this.label.align = 'center';
+	},
 
+
+
+	create: function () {
+		this.createPlayerGroup();
 		//  We need to enable physics on the player
 		this.game.physics.arcade.enable(this.sprite);
 		this.sprite.body.collideWorldBounds = true;
 		//  Our two animations, walking left and right
 		this.addAnimations();
-		
 		//add keyboard motion
 		this.cursors = this.game.input.keyboard.createCursorKeys();
-		
-		(function addLabel(obj){
-			obj.label = obj.game.add.text(obj.sprite.x, 	obj.sprite.y, obj.name, { font: '8pt Helvetica Neue', fill: '#000' });
-		obj.label.align = 'center';
-		})(this);
-		
 	},
-	
+
 	handleHorizontal: function () {
 		if(this.cursors.down.isDown)
-			{
-				this.sprite.body.velocity.y = 250;    	
-				this.sprite.animations.play('up');
-			}
-			else if (this.cursors.up.isDown)
-			{
-				this.sprite.body.velocity.y = -250;
-				this.sprite.animations.play('down');
-			}
-			else{
-				this.sprite.body.velocity.y = 0;	
-			}
+		{
+			this.sprite.body.velocity.y = 250;    	
+			this.sprite.animations.play('up');
+			return true;
+		}
+		else if (this.cursors.up.isDown)
+		{
+			this.sprite.body.velocity.y = -250;
+			this.sprite.animations.play('down');
+			return true;
+		}
+		else{
+			this.sprite.body.velocity.y = 0;
+			return false;
+		}
 	},
-	
+
 	handleVertical: function () {
 		if(this.cursors.left.isDown)
-			{
-				this.sprite.body.velocity.x = -250;
+		{
+			this.sprite.body.velocity.x = -250;
 
-				this.sprite.animations.play('left');
-			}
-			else if(this.cursors.right.isDown)
-			{
-				this.sprite.body.velocity.x = 250;
+			this.sprite.animations.play('left');
+			return true;
+		}
+		else if(this.cursors.right.isDown)
+		{
+			this.sprite.body.velocity.x = 250;
 
-				this.sprite.animations.play('right');
-			}
-			else{
-				this.sprite.body.velocity.x = 0;
-			}
+			this.sprite.animations.play('right');
+			return true;
+		}
+		else{
+			this.sprite.body.velocity.x = 0;
+			return false;
+		}
 	},
-	
+
 	handleStop: function () {
-		if(!(this.cursors.up.isDown || this.cursors.down.isDown || this.cursors.right.isDown || 	this.cursors.left.isDown)){
-				this.sprite.animations.stop();
-				this.sprite.frame = 4;
-			}
+		this.sprite.animations.stop();
+		this.sprite.frame = 4;
 	},
-	
+
 	update: function() {
 		this.label.x = this.sprite.x;
 		this.label.y = this.sprite.y - 3;
-
-		this.handleVertical();
-		this.handleHorizontal();
-		this.handleStop();
+		var vMove = this.handleVertical();
+		var hMove = this.handleHorizontal();
+		
+		if(!vMove && !hMove){
+			this.handleStop();
+		}
 	}
 };
