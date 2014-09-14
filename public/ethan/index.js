9292ff9dfd6782001b6a98a5c518861a37c4c65f
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-09-15 00:58:12 +0400

    adds meta-ness for player names, and action seas

--- public/ethan/index.js
+++ public/ethan/index.js
@@ -1,13 +1,9 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(600, 600, Phaser.AUTO, 'phaser', { preload: preload, create: create, update: update });
 
 function preload() {
-	game.load.image('sky', '/ethan/sky.png');
-	game.load.spritesheet('dude', '/ethan/body-alpha.png', 21, 47);
+	game.load.spritesheet('dude', '/game/assets/dude.png', 32, 48);
 }
-
-var player;
 var people;
-var list = ['doctor', 'nurse', 'patient','patient', 'doctor2'];
 var m = {};
 var states = [];
 var hello;
@@ -15,26 +11,39 @@ var hello;
 var init_player = function(player){
 	game.physics.arcade.enable(player);
 	player.body.collideWorldBounds = true;
-	player.animations.add('backward', [0, 1, 2, 3], 10, true);
-	player.animations.add('left', [4, 5, 6, 7], 10, true);
-	player.animations.add('forward', [8, 9, 10, 11], 10, true);
-	player.animations.add('right', [12, 13, 14, 15], 10, true);
+	player.animations.add('left', [0, 1, 2, 3], 10, true);
+	player.animations.add('right', [5, 6, 7, 8], 10, true);
+	player.animations.add('up', [4], 10, true);
+	player.animations.add('down', [4], 10, true);
+	
+
+	
+};
+
+var addLabel = function(player, name){
+		var label = this.game.add.text(0,0, name, { font: '10px Helvetica Neue', fill: '#000' });
+		label.x = Math.floor((player.width - player.width)*0.5);
+		label.y = Math.floor(player.height);
+		label.align = 'center';
+		player.addChild(label);
 };
 
 var createNPCs = function(){
 	people = new Phaser.Group(game, game.world, 'people', true, true, Phaser.Physics.Arcade);
-	player = game.add.sprite(32, 32, 'dude');
-	 for (var i = 0; i < 2; i++)
+	//player = game.add.sprite(32, 32, 'dude');
+	 for (var i = 0; i < data.players.length; i++)
     {
         var sprite = people.create(game.world.randomX, 	game.world.randomY, 'dude');
 		init_player(sprite);
+		
+		
 	}
 	for(var i = 0; i< people.length; i++){
 		var p = people.getAt(i);
-		console.log(i, p);
-		m[list[i]] = p;
+		m[data.players[i]] = p;
+		addLabel(p, data.players[i]);
 	}
-	people.add(player);
+	//people.add(player);
 };
 hello = {
 	within: function(c1,c2,t){
@@ -58,7 +67,7 @@ hello = {
 	go_to_person: function(name,name2){
 		var npc = this.getnpc(name);
 		var npc2 = this.getnpc(name2);
-		var close_enough = 30;
+		var close_enough = 70;
 		var that = this;
 		return function(){
 			if(that.within(npc, npc2, close_enough) === true){
@@ -90,7 +99,7 @@ hello = {
 	},
 
 	pause_animation: function(npc){
-		npc.animations.play("backward");
+		npc.animations.play("down");
 		npc.animations.stop();
 		npc.body.velocity.x = 0;
 		npc.body.velocity.y = 0;
@@ -133,6 +142,7 @@ hello = {
 		return npc.state_m;
 	},
 	say_pause :function(text,name,time){
+		console.log(this);
 		var npc = this.getnpc(name);
 		var that = this;
 		return {
@@ -192,11 +202,11 @@ hello = {
 			npc.body.velocity.x = 0;
 			if(diffy < 0){
 				npc.body.velocity.y = speed;
-				npc.animations.play("backward");
+				npc.animations.play("down");
 			}
 			else if(diffy > 0){
 				npc.body.velocity.y = -speed;
-				npc.animations.play("forward");
+				npc.animations.play("up");
 			}
 			else{
 				npc.body.velocity.y = 0;
@@ -211,48 +221,23 @@ function create() {
 
 	//  We're going to be using physics, so enable the Arcade Physics system
 	game.physics.startSystem(Phaser.Physics.ARCADE);
-
-	//  A simple background for our game
-	game.add.sprite(0, 0, 'sky');
-
+	game.stage.backgroundColor = 0xffffff;
+    
 	createNPCs();
-	init_player(player);
-	//people.forEach(init_player);
-	
-	
-	states.push(hello['npc_state']('doctor',
-								 [ hello['say_pause']("I am a doctor", 'doctor', 2000),
-								  //hello['go_to_person']('doctor', 'patient'),
-								  hello['say_pause']("(cutting cord...)",'doctor', 2000),
-								 // say_pause("(wrapping bandage...)",'doctor', 2000),
-								  hello['go_to_person']('doctor', 'nurse'),
-								  hello['say_pause']("(instructing nurse...)", 'doctor', 2000),
-								 // pause('doctor',2000)
-								 ]));
-	states.push(hello['npc_state']('nurse',
-							[ hello['say_pause']("I am a nurse.", 'nurse', 2000),
-							 hello['wait_for_action']('doctor',"(instructing nurse...)"),
-							 hello['pause']('nurse', 1000),
-							 hello['say_pause']("(open door...)", 'nurse', 2000),
-							 //go_to_person('nurse', 'patient'),
-							 //say_pause("(call name...)", 'nurse', 2000),
-							//go_to_loc('nurse', function() { return {x:300, y:300}; }),
-							]
-							 ));
-		/*					 
-	states.push(npc_state('patient',
-							  [ say_pause("I am lying in the hospital.", 'patient', 2000),
-							   pause('patient', 5000),
-							   say_pause("Now I am leaving the hospital.", 'patient', 2000),
-							   pause('patient', 1000),
-							  ]));
-	}
-*/
+	//init_player(player);
+
+	data.acting.forEach(function(npc){
+		var npc_actions = [];
+		npc.fns.forEach(function(fn){
+			npc_actions.push((hello[fn.fn]).apply(hello,fn.args));
+		});
+		states.push(hello['npc_state'](npc.name, npc_actions));
+	});
   }
  function update() {
 	 game.physics.arcade.collide(people);
-	 game.physics.arcade.collide(people, player);
+	 //game.physics.arcade.collide(people, player);
 	 states.forEach(function(state){
 		  state.run();
 	 });
-}
+ }
