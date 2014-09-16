292cadc7a834ac0872adf33ce24aed0701b02f7f
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-09-16 21:54:38 +0400

    changes decor placement

--- public/game/Decors.js
+++ public/game/Decors.js
@@ -7,27 +7,31 @@ Decors.prototype = {
 	preload: function() {
 		this.game.load.image('bed', '/game/assets/bed2.png');
 		this.game.load.image('table', '/game/assets/table.jpg');
+		this.game.load.image('bg', '/game/assets/simplebg.jpg');
 	},
 
 	create: function() {
+
+		//background = game.add.tileSprite(0, 0, 600, 600, "bg");
 		//  The platforms group contains the ground and the 2 ledges we can jump on
-		this.platforms = game.add.group();
+		this.platforms = this.game.add.group();
 		this.game.physics.arcade.enable(this.platforms);
 		//  We will enable physics for any object that is created in this group
 		this.platforms.enableBody = true;
 
 		// Here we create the ground.
 		//  Now let's create two ledges
-		for(var i = 0; i < 5; i++){
-			var bed = this.platforms.create((120*i)+10, 400, 'bed');
-			bed.scale.setTo(0.4,0.4);
+		for(var i = 0; i < 4; i++){
+			var bed = this.platforms.create((100*i)+110, 50, 'bed');
+			bed.scale.setTo(0.35,0.35);
 			bed.body.immovable = true;
 		}
-    
-    for(var i = 0; i < 5; i++){
-			var table = this.platforms.create((120*i)+10, 200, 'table');
-			table.scale.setTo(0.4,0.4);
-			table.body.immovable = true;
+    	for(var j = 0; j < 2; j++){
+	    	for(var i = 0; i < 1; i++){
+				var table = this.platforms.create((160*i)+40, (160*j)+ 250, 'table');
+				table.scale.setTo(0.55,0.55);
+				table.body.immovable = true;
+			}
 		}
 	},
 	update: function() {
